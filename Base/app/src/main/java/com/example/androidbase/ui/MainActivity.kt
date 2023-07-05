9a71953be19cd24ae05adbdf0164605f1260fcf2
KimJungYun <wkdrns3918@gmail.com>
2023-07-05 17:50:43 +0900

    메인 액티비와 홈 프레그먼트 데이터 바인딩 설정

--- Base/app/src/main/java/com/example/androidbase/ui/MainActivity.kt
+++ Base/app/src/main/java/com/example/androidbase/ui/MainActivity.kt
@@ -3,10 +3,17 @@ package com.example.androidbase.ui
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.example.androidbase.R
+import com.example.androidbase.databinding.ActivityMainBinding
+import com.example.androidbase.ui.home.HomeFragment
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        supportFragmentManager.beginTransaction().replace(R.id.fragment_top, HomeFragment())
     }
 }
