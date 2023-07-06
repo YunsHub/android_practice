824026d30b133a58cbf5d45deea9b4488e6e91af
KimJungYun <wkdrns3918@gmail.com>
2023-07-06 15:07:48 +0900

    feat : retrofit 객체 applicationClass에 생성

--- Base/app/src/main/java/com/example/androidbase/ApplcationClass.kt
+++ Base/app/src/main/java/com/example/androidbase/ApplcationClass.kt
@@ -1,4 +1,18 @@
 package com.example.androidbase
 
-class ApplcationClass {
+import android.app.Application
+import com.example.androidbase.utils.BASE_URL
+import com.google.gson.GsonBuilder
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+
+class ApplcationClass : Application() {
+    companion object {
+        private val retrofit: Retrofit by lazy {
+            Retrofit.Builder()
+                .baseUrl(BASE_URL)
+                .addConverterFactory(GsonConverterFactory.create())
+                .build()
+        }
+    }
 }
---
+++ Base/app/src/main/java/com/example/androidbase/ui/booklist/BookListViewModel.kt
@@ -0,0 +1,4 @@
+package com.example.androidbase.ui.booklist
+
+class BookListViewModel {
+}
