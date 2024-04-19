2accdef32dd799e18ca62bf2fa75d51b9b1ecaa2
BRIGHTRIN\dpfls <wkdrns3918@gmail.com>
2024-04-19 14:20:32 +0900

    chapter1 : 간단한 버튼 클릭 시 카운트 증가 구현

--- ComposeBasic/app/src/main/java/com/example/composebasic/MainActivity.kt
+++ ComposeBasic/app/src/main/java/com/example/composebasic/MainActivity.kt
@@ -1,13 +1,19 @@
 package com.example.composebasic
 
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.Button
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.MutableState
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.composebasic.ui.theme.ComposeBasicTheme
@@ -17,9 +23,14 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             ComposeBasicTheme {
-                // A surface container using the 'background' color from the theme
+
+                var clickCount: MutableState<Int> = remember { mutableStateOf(0) }
+
                 Surface(modifier = Modifier.fillMaxSize(), color = MaterialTheme.colorScheme.background) {
-                    Greeting("Android")
+                    Greeting("Android", clickCount.value, onClicked = {
+                        Log.d("TAG", "onCreate: 클릭")
+                        clickCount.value += 1
+                    })
                 }
             }
         }
@@ -27,17 +38,21 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-            text = "Hello $name!",
-            modifier = modifier
-    )
+fun Greeting(name: String, clickCount: Int, onClicked: () -> Unit) {
+    Column {
+        Text(text = "Hello $name!")
+        Text(text = "클릭된 카운트 : $clickCount")
+        Button(onClicked) {
+            Text(text = "클릭해주세요.")
+        }
+    }
+
 }
 
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    ComposeBasicTheme {
-        Greeting("Android")
-    }
-}
+//@Preview(showBackground = true)
+//@Composable
+//fun GreetingPreview() {
+//    ComposeBasicTheme {
+//        Greeting("Android")
+//    }
+//}
