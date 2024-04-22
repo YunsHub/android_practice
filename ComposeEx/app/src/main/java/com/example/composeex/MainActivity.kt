dff59dfc9f9877079ad1d3700a03d6f91a8243bb
BRIGHTRIN\dpfls <wkdrns3918@gmail.com>
2024-04-22 16:43:54 +0900

    Android Compose : Text 단락 Example

--- ComposeEx/app/src/main/java/com/example/composeex/MainActivity.kt
+++ ComposeEx/app/src/main/java/com/example/composeex/MainActivity.kt
@@ -3,13 +3,35 @@ package com.example.composeex
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.ExperimentalFoundationApi
+import androidx.compose.foundation.basicMarquee
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
+import androidx.compose.material3.LocalTextStyle
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.geometry.Offset
+import androidx.compose.ui.graphics.Brush
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.Shadow
+import androidx.compose.ui.text.PlatformTextStyle
+import androidx.compose.ui.text.SpanStyle
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.buildAnnotatedString
+import androidx.compose.ui.text.font.FontStyle
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.LineHeightStyle
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.text.withStyle
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.em
+import androidx.compose.ui.unit.sp
 import com.example.composeex.ui.theme.ComposeExTheme
 
 class MainActivity : ComponentActivity() {
@@ -19,7 +41,7 @@ class MainActivity : ComponentActivity() {
             ComposeExTheme {
                 // A surface container using the 'background' color from the theme
                 Surface(modifier = Modifier.fillMaxSize(), color = MaterialTheme.colorScheme.background) {
-                    Greeting("Android")
+
                 }
             }
         }
@@ -27,10 +49,24 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
+fun MyCenterText() {
     Text(
-        text = "Hello $name!",
-        modifier = modifier
+        text = "Hello World, Compose",
+        fontSize = 20.sp,
+        textAlign = TextAlign.Center,
+        modifier = Modifier.width(100.dp)
+    )
+}
+
+@Composable
+fun MyHeightText() {
+    Text(
+        text = "Hello World, ComposeHello World, ComposeHello World, ComposeHello World, ComposeHello World, ComposeHello World, Compose",
+        style = LocalTextStyle.current.merge(
+            TextStyle(
+                lineHeight = 2.5.em
+            )
+        )
     )
 }
 
@@ -38,6 +74,9 @@ fun Greeting(name: String, modifier: Modifier = Modifier) {
 @Composable
 fun GreetingPreview() {
     ComposeExTheme {
-        Greeting("Android")
+        // A surface container using the 'background' color from the theme
+        Surface(modifier = Modifier.fillMaxSize(), color = MaterialTheme.colorScheme.background) {
+            MyHeightText()
+        }
     }
 }
