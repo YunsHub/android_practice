533d4ab8c41b9c6aaecee57dd2dac21c22997161
BRIGHTRIN\dpfls <wkdrns3918@gmail.com>
2024-04-22 17:37:17 +0900

    Android Compose : Button, Toast Example

--- ComposeEx/app/src/main/java/com/example/composeex/MainActivity.kt
+++ ComposeEx/app/src/main/java/com/example/composeex/MainActivity.kt
@@ -1,15 +1,23 @@
 package com.example.composeex
 
 import android.os.Bundle
+import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.width
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.example.composeex.ui.theme.ComposeExTheme
 
 class MainActivity : ComponentActivity() {
@@ -18,8 +26,11 @@ class MainActivity : ComponentActivity() {
         setContent {
             ComposeExTheme {
                 // A surface container using the 'background' color from the theme
-                Surface(modifier = Modifier.fillMaxSize(), color = MaterialTheme.colorScheme.background) {
-                    Greeting("Android")
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                    MyButtonEx("Android")
                 }
             }
         }
@@ -27,17 +38,28 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
+fun MyButtonEx(name: String, modifier: Modifier = Modifier) {
+    val context = LocalContext.current
+
+    Button(
+        onClick = {
+            Toast.makeText(context, "클릭", Toast.LENGTH_LONG).show()
+        },
+        colors = ButtonDefaults.buttonColors(
+            containerColor = Color.Yellow,
+            contentColor = Color.Blue
+        ),
+        modifier = Modifier.width(120.dp).height(100.dp)
     )
+    {
+        Text("버튼입니다. ")
+    }
 }
 
 @Preview(showBackground = true)
 @Composable
 fun GreetingPreview() {
     ComposeExTheme {
-        Greeting("Android")
+        MyButtonEx("Android")
     }
 }
